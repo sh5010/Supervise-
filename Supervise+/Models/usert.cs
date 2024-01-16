3c6848e0e321bbac175612bdac73f68aed905cc7
rnhalz0 <rnhalz0@gmail.com>
2024-01-16 13:09:16 +0300

    .

---
+++ Supervise+/Models/usert.cs
@@ -0,0 +1,12 @@
+﻿namespace Supervise_.Models
+{
+    public class usert
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public string Role { get; set; }
+    }
+}
