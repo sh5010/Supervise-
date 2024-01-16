de7077e5a1507f807d6db6368b449bcdc91b92ab
Samaher22 <Samahers122@gamil.com>
2024-01-16 09:16:24 +0300

    s

--- Supervise+/Controllers/HomeController.cs
+++ Supervise+/Controllers/HomeController.cs
@@ -22,50 +22,7 @@ namespace Supervise_.Controllers
         {
             return View();
         }
-
-        [HttpPost, ActionName("login")]
-        public async Task<IActionResult> login(string na, string pa)
-        {
-            var builder = WebApplication.CreateBuilder();
-            string conStr = builder.Configuration.GetConnectionString("Supervise+Context");
-            SqlConnection conn1 = new SqlConnection(conStr);
-            string sql;
-            sql = "SELECT * FROM User where name ='" + na + "' and  pass ='" + pa + "' ";
-            SqlCommand comm = new SqlCommand(sql, conn1);
-            conn1.Open();
-            SqlDataReader reader = comm.ExecuteReader();
-
-            if (reader.Read())
-            {
-                string id = Convert.ToString((int)reader["Id"]);
-                string na1 = (string)reader["name"];
-                string ro = (string)reader["role"];
-                HttpContext.Session.SetString("userid", id);
-                HttpContext.Session.SetString("Name", na1);
-                HttpContext.Session.SetString("Role", ro);
-                reader.Close();
-                conn1.Close();
-                if (ro == "Student")
-                {
-                    return RedirectToAction("student_home","Home");
-                }
-                else if (ro == "facultys")
-                {
-                    return RedirectToAction("facultys_home","Home");
-                }
-                else if (ro == "graduation_project_committee")
-                {
-                    return RedirectToAction("graduation_project_committee","Home");
-                }
-            }
-            else
-            {
-                ViewData["Message"] = "wrong user name password";
-                return View();
-            }
-        }
-
-
+        
         public IActionResult Privacy()
         {
             return View();
